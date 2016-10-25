c40bd63fd4a2a8032c899310649c4c9639e94a79
Jaume Mila <jmila@leadtech.com>
2016-10-25 12:12:47 +0200

    Change minor version beta

--- proxies/settings.template.py
+++ proxies/settings.template.py
@@ -1,4 +1,4 @@
-SH_TOR_DIR = "/home/jaume/Documents/workspace/scrap-utils/proxies/torlib"
+SH_TOR_DIR = "/path/to/scrap-utils/proxies/torlib"
 SH_TOR_START = "tor_start.sh"
 SH_TOR_RELOAD = "tor_reload_torrc.sh"
 SH_TOR_EXIT = "tor_exit.sh"
